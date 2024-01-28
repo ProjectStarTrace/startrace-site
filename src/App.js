cc39268142f72157555e1dfe146336401be5dc65
Caleb Tipton <greensky14@gmail.com>
2024-01-28 12:52:22 -0600

    got the header unified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Downloads from './Downloads'; // Import the Downloads component
+import Leaderboard from './Leaderboard'; // Import the Leaderboard component
+
 import './App.css';
 import starsImage from './assets/stars.jpg';
 import startraceLogo from './assets/startrace.png'; // Import the logo image
@@ -37,11 +41,11 @@ const Header = () => {
           <span></span>
         </div>
         {isDropdownOpen && (
-          <div className="dropdown-content">
-            <a href="#">Open StarScout</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
+        <div className="dropdown-content">
+          <a href="https://startrace.dev/scope">Open StarScope</a>
+          <a href="/downloads">Downloads</a>
+          <a href="/leaderboard">Leaderboard</a>
+        </div>
         )}
       </div>
 
