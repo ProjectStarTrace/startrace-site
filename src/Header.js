7460da03f52a335435e3565de55ff916169b056b
Caleb Tipton <greensky14@gmail.com>
2024-01-28 13:45:53 -0600

    updated starscope link to have proper routing

--- src/Header.js
+++ src/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
           </div>
           {isDropdownOpen && (
           <div className="dropdown-content">
-            <a href="https://startrace.dev/scope">Open StarScope</a>
+            <a href="https://scope.startrace.dev/">Open StarScope</a>
             <a href="/downloads">Downloads</a>
             <a href="/leaderboard">Leaderboard</a>
           </div>
