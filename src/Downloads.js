97f6dce2e856ffcdf01e6fc30ad467c04257b78b
CalebJTipton <caleb@tipton.cloud>
2024-02-02 14:46:10 -0600

    updates to guide

--- src/Downloads.js
+++ src/Downloads.js
@@ -1,8 +1,12 @@
 // Downloads.js
 import React from 'react';
 import Header from './Header'; // Import the Header component
-import starScoutDownloadImage from './assets/starscoutDownload1.png'; // Make sure the path matches
+import starScoutDownloadImage1 from './assets/starScoutDownload1.png'; // Make sure the path matches
 import './Downloads.css';
+import rasPiImage from './assets/raspPi.png';
+import microSDImage from './assets/microSD.png';
+import balena from './assets/balena.png'; // Make sure the path matches
+import webpage from './assets/webpage.svg'; // Import the StarScope image
 
 const Downloads = () => {
   return (
@@ -10,7 +14,7 @@ const Downloads = () => {
       <Header />
       {/* Full-width image at the top */}
       <div className="full-width-image-container">
-        <img src={starScoutDownloadImage} alt="StarScout Download" />
+        <img src={starScoutDownloadImage1} alt="StarScout Download" />
       </div>
 
       {/* Download options */}
@@ -33,8 +37,24 @@ const Downloads = () => {
       {/* Setup guide */}
       <div className="setup-guide">
         <h2>Setting Up Your Raspberry Pi with StarScout</h2>
-        <p>Follow these easy steps to get started with StarScout on your Raspberry Pi:</p>
-        {/* Include your guide steps here */}
+        <div className="setup-steps">
+          <div className="step">
+            <img src={microSDImage} alt="MicroSD Card" />
+            <p>1) Download the Image and plug a 32GB or larger MicroSD card into your computer.</p>
+          </div>
+          <div className="step">
+            <img src={balena} alt="Balena Etcher" />
+            <p>2) Download Balena Etcher and load the image file into the program, selecting the MicroSD card.</p>
+          </div>
+          <div className="step">
+            <img src={rasPiImage} alt="Raspberry Pi" />
+            <p>3) Once flashing is complete, plug the SD Card into the Raspberry Pi and connect it to power and network.</p>
+          </div>
+          <div className="step">
+            <img src={webpage} alt="StarScout Setup" />
+            <p>4) Navigate to StarScout.local and follow the on-screen prompts for setup.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
