da330de97436334e15ce1dc5842d8046d53ee3c5
CalebJTipton <caleb@tipton.cloud>
2024-02-04 10:48:39 -0600

    added short desriptions and stylized

--- src/Team.js
+++ src/Team.js
@@ -14,12 +14,12 @@ const Team = () => {
         <div className="team-member">
           <img src={calebImage} alt="Caleb Tipton" />
           <h3>Caleb Tipton</h3>
-          <p>A short description about Caleb.</p>
+          <p>Caleb Tipton is a full-stack software engineer and IT Administrator, currently advancing his studies at the University of North Dakota's College of Engineering and Mines. His entrepreneurial journey has led to the founding of Carbon ATM LLC, an environmental sensor company, and Tipton IT LLC, an IT consulting firm, both of which are based in Houston, Texas. Caleb's professional experience extends to consulting for startups, where he has contributed his expertise in software development, user interface design, and digital marketing to support their growth. </p>
         </div>
         <div className="team-member">
           <img src={andrewImage} alt="Andrew Woods" />
           <h3>Andrew Woods</h3>
-          <p>A short description about Andrew.</p>
+          <p>Andrew Woods is a dedicated student at the University of North Dakota, specializing in back-end development and machine learning. His academic journey is marked by a notable collaboration on a project for the US Department of Defense, facilitated through the university. This collaboration not only highlights his technical expertise but also his ability to apply his knowledge to real-world applications, contributing to significant projects at a national level.</p>
         </div>
       </div>
     </div>
